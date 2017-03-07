af6deade469b5053eb477caed8a0b62cf272eae9
bruno-rocha-movile <bruno.rocha@movile.com>
2017-03-07 16:07:03 -0300

    prints

--- lib/slack_poster.rb
+++ lib/slack_poster.rb
@@ -17,6 +17,10 @@ class SlackPoster
   def send_request(message)
   	puts message
   	uri = URI.parse("#{@webhook_url}")
+    puts(uri)
+    puts("#{@mood_hash[:username]}")
+    puts("#{@mood_hash[:icon_emoji]}")
+    puts("#{message}")
   	response = Net::HTTP.post_form(uri, {"username" => "#{@mood_hash[:username]}", "icon_emoji" => "#{@mood_hash[:icon_emoji]}", "text" => "#{message}"})
   	puts response
   end
